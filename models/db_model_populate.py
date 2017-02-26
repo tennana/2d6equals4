f7fb375194bddb2bea598a617fb288e36669ec8a
Tennana <tennana@nifty.com>
2017-02-27 03:41:04 +0900

    delete test data

--- models/db_model_populate.py
+++
@@ -1,6 +0,0 @@
-from gluon.contrib.populate import populate
-if db(db.auth_user).isempty():
-     populate(db.auth_user,10)
-     populate(db.participant,10)
-     populate(db.user,10)
-     populate(db.wishforgametable,10)
