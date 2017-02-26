2c80488064cbb7f578444898aebb64d85902df5a
Tennana <tennana@nifty.com>
2017-02-27 01:11:46 +0900

    辞書ファイル定義

---
+++ languages/default.py
@@ -0,0 +1,15 @@
+# coding: utf8
+{
+'Abstract' : '卓概要(掲載用)',
+'Category' : '参加区分',
+'Optional Assist' : 'えびのしっぽ',
+'Optional Closing Party' : '懇親会参加',
+'Remark' : '備考',
+'SystemName' : '立卓予定システム',
+'Optional Closing Party': '懇親会参加',
+'MinimumNumber': '対応可能人数(最小)',
+'MaximumNumber': '対応可能人数(最大)',
+'Level': '対応レベル',
+'Belongings': '必須の持ち物',
+'Are you sure you want to delete this object?': 'チェックして送信すると参加申し込みがキャンセルされます。よろしいですか?'
+}
