830292e79a03f8644d0378318d504600888f1dc4
Tennana <tennana@nifty.com>
2017-02-26 16:34:16 +0900

    model_fixed

--- models/db_model.py
+++ models/db_model.py
@@ -1,23 +1,6 @@
 ### we prepend t_ to tablenames and f_ to fieldnames for disambiguity
 
 ########################################
-db.define_table('users',
-    Field('name', type='string',
-          label=T('Name')),
-    Field('email_authid', type='reference auth_user',
-          label=T('Email Authid')),
-    Field('twitter', type='reference auth_user',
-          label=T('Twitter')),
-    Field('google', type='reference auth_user',
-          label=T('Google')),
-    Field('oAuth', type='reference auth_user',
-          label=T('oAuth')),
-    auth.signature,
-    migrate=settings.migrate)
-
-db.define_table('t_user_archive',db.users,Field('current_record','reference users',readable=False,writable=False))
-
-########################################
 db.define_table('convention',
     Field('name', type='string', length=320,
           label=T('Name')),
@@ -29,17 +12,17 @@ db.define_table('convention',
           label=T('PlayerDueDate')),
     Field('openingDate', type='datetime', required=True,
           label=T('Date')),
-    Field('adminuserid', type='reference users', required=True,
-          label=T('AdminUserID')),
     auth.signature,
     migrate=settings.migrate)
 
-db.define_table('t_convention_archive',db.convention,Field('current_record','reference convention',readable=False,writable=False))
+db.convention._enable_record_versioning()
 
 ########################################
+dic_gameLevel = {0:'TRPG初心者OK',1:'システム初心者OK',2:'経験者向け',3:'濃い'}
+
 db.define_table('gameTable',
-    Field('name', type='string', required=True, length=512 ,
-          label=T('Name')),
+    Field('tableName', type='string', required=True, length=512 , writable=False,
+          label=T('tableName')),
     Field('systemname', type='string', required=True, length=128 ,
           label=T('SystemName')),
     Field('minimumnumber', type='integer', required=True,
@@ -47,40 +30,43 @@ db.define_table('gameTable',
     Field('maximumnumber', type='integer', required=True,
           label=T('MaximumNumber')),
     Field('gameLevel', type='integer', required=True,
+          requires = IS_IN_SET(dic_gameLevel,zero=None),
           label=T('Level')),
-    Field('belongings', type='text',
+    Field('belongings', type='string',
           label=T('Belongings')),
     Field('abstract', type='text', required=True,
           label=T('Abstract')),
-    Field('remark', type='text',
-          label=T('Remark')),
     Field('convention_id', type='reference convention',
           label=T('Convention Id')),
-    Field('gamemaster_id', type='reference users',
-          label=T('GameMaster Id')),
     auth.signature,
     migrate=settings.migrate)
 
-db.define_table('t_gametable_archive',db.gameTable,Field('current_record','reference gameTable',readable=False,writable=False))
+db.gameTable._enable_record_versioning()
 
 ########################################
+
+dic_category = {0:'プレイヤー',1:'ゲームマスター',2:'ヘルP'}
+
 db.define_table('participant',
     Field('convention', type='reference convention'),
-    Field('status', type='integer', notnull=True, default = 0,
+    Field('status', type='integer', notnull=True, default = -1,writable=False,
+          requires = IS_IN_SET({'-1':'保留中','0':'参加受付','1':'キャンセル待ち','2':'キャンセル'},zero=None),
           label=T('Status')),
     Field('category', type='integer', notnull=True, default = 0,
+          represent=lambda category: dic_category[category] or '【削除済み】',
+          requires = IS_IN_SET(dic_category,zero=None),
           label=T('Category')),
-    Field('optional_assist', type='integer',default = 0,
+    Field('optional_assist', type='boolean',default = 0,
           label=T('Optional Assist')),
-    Field('optional_closing_party', type='integer', default = 0,
+    Field('optional_closing_party', type='boolean', default = 0,
           label=T('Optional Closing Party')),
-    Field('decisionToPlayer', type='reference gameTable'),
+    Field('decisionToPlayer', type='reference gameTable', writable=False, readable=False),
     Field('remark', type='text',
           label=T('Remark')),
     auth.signature,
     migrate=settings.migrate)
 
-db.define_table('t_participant_archive',db.participant,Field('current_record','reference participant',readable=False,writable=False))
+db.participant._enable_record_versioning()
 
 ########################################
 db.define_table('wishforgametable',
@@ -93,4 +79,5 @@ db.define_table('wishforgametable',
     auth.signature,
     migrate=settings.migrate)
 
-db.define_table('t_wishforgametable_archive',db.wishforgametable,Field('current_record','reference wishforgametable',readable=False,writable=False))
+db.wishforgametable._enable_record_versioning()
+
--- models/db_model_populate.py
+++ models/db_model_populate.py
@@ -1,6 +1,6 @@
 from gluon.contrib.populate import populate
 if db(db.auth_user).isempty():
      populate(db.auth_user,10)
-     populate(db.t_participant,10)
-     populate(db.t_user,10)
-     populate(db.t_wishforgametable,10)
+     populate(db.participant,10)
+     populate(db.user,10)
+     populate(db.wishforgametable,10)
