efc79f2d41c59c6da20e08a177e1f8e230c715db
Tennana <tennana@nifty.com>
2017-03-17 00:39:51 +0900

    メニューにTOPを追加

--- models/menu.py
+++ models/menu.py
@@ -4,5 +4,6 @@ response.meta.author = '%(author)s <%(author_email)s>' % settings
 response.meta.keywords = settings.keywords
 response.meta.description = settings.description
 response.menu = [
+(T('TOP'),URL('default','index')==URL(),URL('default','index'),[]),
 (T('Participant Regist'),URL('default','participant_manage')==URL(),URL('default','participant_manage'),[]),
 ]
